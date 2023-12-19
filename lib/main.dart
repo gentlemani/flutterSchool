0ab1618e1ba1445f514c53dab6141a0f7af59dcc
gentlemani <robin202010@hotmail.com>
2023-12-19 17:02:05 -0600

    prueba de inicializacion firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:eatsily/firebase_options.dart';
 import 'package:eatsily/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
