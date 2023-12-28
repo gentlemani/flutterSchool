182779f8591cf2266f8e59aa67030e04e1e0c783
Coddy6 <asprans77@gmail.com>
2023-12-28 14:47:30 -0600

    Diseño e implementacion del archivo de account, para el perfil del usuario. Asignar icon a la aplicación

--- lib/Interface_pages/account_page.dart
+++ lib/Interface_pages/account_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
-  const AccountPage({Key? key}) : super(key: key);
+  const AccountPage({super.key});
 
   @override
   State<AccountPage> createState() => _AccountPageState();
@@ -12,13 +12,34 @@ class _AccountPageState extends State<AccountPage> {
   Widget build(BuildContext context) {
     return ClipRRect(
         child: Container(
-            padding: const EdgeInsets.all(2),
+            padding: const EdgeInsets.all(0),
             child: Scaffold(
-              appBar: AppBar(
-                title: const Center(child: Text('Bienvenido a la')),
-              ),
-              body: const Center(child: Text('d')),
-              backgroundColor: const Color.fromARGB(255, 54, 114, 244),
-            )));
+                appBar: AppBar(
+                  titleTextStyle: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                  backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                  title: const Text('Perfil'),
+                  automaticallyImplyLeading: false,
+                  actions: [
+                    IconButton(
+                      iconSize: 30,
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        // Acción al presionar el botón
+                      },
+                    ),
+                    IconButton(
+                      iconSize: 30,
+                      icon: const Icon(Icons.settings),
+                      onPressed: () {
+                        // Otra acción
+                      },
+                    ),
+                  ],
+                ),
+                body: const Column(),
+                backgroundColor: const Color.fromARGB(255, 255, 255, 255))));
   }
 }
