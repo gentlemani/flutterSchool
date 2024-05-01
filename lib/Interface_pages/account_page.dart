b59d77418266787c30b612f79be847d4fcac4135
gentlemani <robin202010@hotmail.com>
2024-04-30 23:34:18 -0600

    Prueba probar ajuste en visualización boton sign out.

--- lib/Interface_pages/account_page.dart
+++ lib/Interface_pages/account_page.dart
@@ -95,59 +95,62 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        child: Container(
-      padding: const EdgeInsets.all(0),
-      child: Scaffold(
-        appBar: AppBar(
-          titleTextStyle: const TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          title: const Text('Perfil'),
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              iconSize: 30,
-              icon: const Icon(Icons.edit),
-              onPressed: () {
-                // Acción al presionar el botón
-              },
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Perfil',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
             ),
-            IconButton(
-              iconSize: 30,
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                // Otra acción
-              },
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-          ],
-        ),
-        body: Container(
+            backgroundColor: Colors.white,
+            automaticallyImplyLeading: false,
+            actions: [
+              IconButton(
+                iconSize: 30,
+                icon: const Icon(Icons.edit),
+                onPressed: () {
+                  // Handle edit button press
+                },
+              ),
+              IconButton(
+                iconSize: 30,
+                icon: const Icon(Icons.settings),
+                onPressed: () {
+                  // Handle settings button press
+                },
+              ),
+            ],
+          ),
+          body: Container(
             padding: const EdgeInsets.only(top: 40),
             alignment: Alignment.topCenter,
-            child: Column(children: [
-              GestureDetector(
-                onTap: () {
-                  getImage();
-                },
-                child: CircleAvatar(
-                  radius: 80,
-                  backgroundColor: Colors.white,
-                  backgroundImage: _imageFile != null
-                      ? FileImage(_imageFile!)
-                      : const AssetImage('assets/iconoP.png') as ImageProvider<
-                          Object>?, // Imagen predeterminada si no hay ninguna seleccionada
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: getImage,
+                  child: CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.white,
+                    backgroundImage: _imageFile != null
+                        ? FileImage(_imageFile!)
+                        : const AssetImage('assets/iconoP.png')
+                            as ImageProvider<Object>?,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 450,
-              ),
-              _buttom(context)
-            ])),
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                const SizedBox(height: 450), // Adjust spacing as needed
+                _buttom(context), // Assuming this is a custom function/widget
+              ],
+            ),
+          ),
+          backgroundColor: Colors.white,
+        ),
       ),
-    ));
+    );
   }
 }
