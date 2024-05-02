d8af1ea2708c14f849db47ce68091ef13f911e19
Coddy6 <asprans77@gmail.com>
2024-05-01 19:32:17 -0600

    Interfaces, reacomodo de archivos y nombres

---
+++ lib/Interface_pages/home_pages/dish_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DishHome extends StatefulWidget {
+  const DishHome({super.key});
+
+  @override
+  State<DishHome> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<DishHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(backgroundColor: Colors.amber);
+  }
+}
