1f9f2fec7b7f72a4083d73bae4f61e5721c9ed07
Coddy6 <asprans77@gmail.com>
2024-08-06 20:07:07 -0600

    Avances del diseño de la primera pestaña de la interfaz.

--- lib/Interface_pages/home_pages/dish_home.dart
+++ lib/Interface_pages/home_pages/dish_home.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+//Constant
+const double kPaddingValue = 18.0;
+const double kImageWidth = 250.0;
+const double kTextFontSize = 25.0;
+const double kImageHeight = 180.0;
+const double kBorderWidth = 3.0;
+const double kBorderRadius = 12.0;
+const Color kBackgroundColor = Color.fromARGB(219, 155, 97, 200);
+const Color kBorderColor = Color.fromARGB(255, 7, 188, 64);
+const Color kShadowColor = Color.fromARGB(255, 54, 50, 50);
+
 class DishHome extends StatefulWidget {
   const DishHome({super.key});
 
@@ -24,23 +35,16 @@ class _DishHomeState extends State<DishHome> {
   Widget beastMeals() {
     return Column(
       children: [
-        const Padding(padding: EdgeInsets.all(18.0)),
-        Expanded(
-            child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [foodInformation(), foodInformation()],
-        )),
-        const Text(
-          "Top rating",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        const Padding(padding: EdgeInsets.all(12.0)),
+        const Padding(padding: EdgeInsets.all(kPaddingValue)),
+        builHeaderText(),
+        const Padding(padding: EdgeInsets.all(kPaddingValue)),
         Expanded(
           child: ListWheelScrollView(
+            physics: const ClampingScrollPhysics(),
             itemExtent: 300,
             diameterRatio: 5,
             useMagnifier: true,
-            magnification: 1.1,
+            magnification: 1.22,
             children: [
               foodInformation(),
               foodInformation(),
@@ -61,55 +65,65 @@ class _DishHomeState extends State<DishHome> {
               "Nueces",
               style: TextStyle(fontSize: 20),
             ),
-            Image.asset(
-              imagePath,
-              width: 300, // Ajusta el tamaño según lo necesites
-              height: 180,
-              alignment: Alignment.center,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 8),
-            const SizedBox(
-              width: 300,
-              child: Text(
-                "Disfruta de toda la nutricion con estas nueces de buen gusto",
-                textAlign: TextAlign.center, // Centrar el texto
-                overflow: TextOverflow
-                    .ellipsis, // Opcional: Muestra "..." si es demasiado largo
-                maxLines: 3, // Limita el texto a un máximo de 3 líneas
-                style: TextStyle(fontSize: 14),
-              ),
-            )
+            FormatImage(imagePath: imagePath),
+            const SizedBox(height: 8), //space between text and image
+            buildDescriptionText()
           ],
         ),
-        const SizedBox(width: 10),
-        const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('3'),
-            Icon(Icons.thumb_up_alt_outlined),
-            // Aquí podrías agregar más widgets si lo deseas, como íconos
-          ],
-        ),
+        const SizedBox(width: 10), //space between image and icons
+        buildLikesSection(),
         const SizedBox(
           width: 10,
-        ),
-        const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('2'),
-            Icon(
-              Icons.thumb_down_alt_outlined,
-            )
-          ],
-        )
+        ), //space between the two icons
+        buildDislikesSection()
       ],
     );
   }
 
-  Widget recommended() {
-    return ListView(
-      scrollDirection: Axis.horizontal,
+  Widget builHeaderText() {
+    return const Text(
+      "!Recomendación a tu gusto¡",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: kTextFontSize,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget buildDescriptionText() {
+    return const SizedBox(
+      width: kImageWidth, //text length with the image
+      child: Text(
+        "Praesent venenatis laoreet dui sed euismod. Fusce sed cursus sem, at posuere ex. Nulla vel ligula justo. Vivamus condimentum eros in erat tincidunt sollicitudin.",
+        textAlign: TextAlign.center, // Center the text
+        overflow:
+            TextOverflow.ellipsis, // Optional: Sample "..." If it's too long
+        maxLines: 3, // limit The Text To A Maximum Of 3 Lines
+        style: TextStyle(fontSize: 14),
+      ),
+    );
+  }
+
+  Widget buildLikesSection() {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('3'),
+        Icon(Icons.thumb_up_alt_outlined),
+      ],
+    );
+  }
+
+  Widget buildDislikesSection() {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('2'),
+        Icon(
+          Icons.thumb_down_alt_outlined,
+        )
+      ],
     );
   }
 
@@ -124,7 +138,53 @@ class _DishHomeState extends State<DishHome> {
         body: Center(
           child: beastMeals(),
         ),
-        backgroundColor: const Color.fromARGB(219, 155, 97, 200));
-    //backgroundColor: Color.fromARGB(0, 255, 193, 7));
+        backgroundColor: kBackgroundColor);
   }
+}
+
+class FormatImage extends StatelessWidget {
+  const FormatImage({
+    super.key,
+    required this.imagePath,
+  });
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 260, // Slightly larger than image to accommodate the border
+      height: 190, // Slightly larger than image to accommodate the border
+      decoration: boxDecoration(),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(
+            kBorderRadius), // Clip the image inside rounded corners
+        child: Image.asset(
+          imagePath,
+          width: kImageWidth,
+          height: kImageHeight,
+          alignment: Alignment.center,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+BoxDecoration boxDecoration() {
+  return BoxDecoration(
+    border: Border.all(
+      color: kBorderColor, // Border color
+      width: kBorderWidth, // Border width
+    ),
+    borderRadius: BorderRadius.circular(kBorderRadius), // Rounded corners
+    boxShadow: [
+      BoxShadow(
+        color: kShadowColor.withOpacity(0.5),
+        spreadRadius: 2,
+        blurRadius: 5,
+        offset: const Offset(0, 3), // Shadow position
+      ),
+    ],
+  );
 }
