a4657ce1eb4c4addc4f33dc010d47b7228309409
Coddy6 <asprans77@gmail.com>
2024-09-25 19:18:14 -0600

    Lista de preferencias en dieta, seleccion

--- lib/Interface_pages/home_pages/account_pages/preferences_account.dart
+++ lib/Interface_pages/home_pages/account_pages/preferences_account.dart
@@ -1,24 +1,41 @@
-import 'package:eatsily/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:eatsily/constants/constants.dart';
 
 class PreferencesAccount extends StatefulWidget {
   const PreferencesAccount({super.key});
 
-  @override
   @override
   State<PreferencesAccount> createState() => PreferencesAccountState();
 }
 
 class PreferencesAccountState extends State<PreferencesAccount> {
   List<String> preferences = [
-    'Opción 1',
-    'Opción 2',
-    'Opción 3',
-    'Opción 4',
-    'Opción 5',
+    'Omnívoro',
+    'Vegano',
+    'Vegetariano',
+    'Flexitariana',
+    'Pescetariana',
+    'Ovo-lacto Vegetariana',
+    'Carnívora'
   ];
 
-  String searchQuery = '';
+  // Información de cada preferencia
+  Map<String, String> preferenceInfo = {
+    'Omnívoro':
+        'La dieta omnívora incluye tanto alimentos de origen vegetal como de origen animal. Es la más amplia y variada de todas, ya que permite el consumo de todos los grupos de alimentos.',
+    'Vegano':
+        'La dieta vegana excluye completamente cualquier producto de origen animal, enfocándose solo en alimentos de origen vegetal.',
+    'Vegetariano':
+        'La dieta vegetariana excluye carnes y pescados, pero permite algunos productos animales, como los lácteos y los huevos.',
+    'Flexitariana':
+        'Es una dieta vegetariana flexible que permite el consumo ocasional de carne o pescado. Se enfoca mayormente en alimentos vegetales, pero sin excluir completamente los productos animales.',
+    'Pescetariana':
+        'Esta dieta es similar a la vegetariana, pero incluye pescado y mariscos.',
+    'Ovo-lacto Vegetariana':
+        'Es una variante del vegetarianismo que incluye productos lácteos y huevos, pero excluye la carne y el pescado.',
+    'Carnívora':
+        'La dieta carnívora se enfoca exclusivamente en alimentos de origen animal, excluyendo completamente los vegetales.'
+  };
 
   Map<String, bool> selectedPreferences = {};
 
@@ -30,21 +47,25 @@ class PreferencesAccountState extends State<PreferencesAccount> {
     }
   }
 
-  void updateSearchQuery(String newQuery) {
-    setState(() {
-      searchQuery = newQuery;
-    });
-  }
-
-  List<String> filteredPreferences() {
-    if (searchQuery.isEmpty) {
-      return preferences;
-    } else {
-      return preferences
-          .where(
-              (pref) => pref.toLowerCase().contains(searchQuery.toLowerCase()))
-          .toList();
-    }
+  void showPreferenceInfo(String preference) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(preference),
+          content:
+              Text(preferenceInfo[preference] ?? 'Información no disponible.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cerrar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -61,26 +82,36 @@ class PreferencesAccountState extends State<PreferencesAccount> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Buscar',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.search),
-              ),
-              onChanged: updateSearchQuery,
-            ),
-            const SizedBox(height: 20),
             Expanded(
               child: ListView(
-                children: filteredPreferences().map((preference) {
-                  return CheckboxListTile(
-                    title: Text(preference),
-                    value: selectedPreferences[preference],
-                    onChanged: (bool? newValue) {
-                      setState(() {
-                        selectedPreferences[preference] = newValue!;
-                      });
-                    },
+                children: preferences.map((preference) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 152, 102, 102)),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: CheckboxListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(preference),
+                          IconButton(
+                            icon: const Icon(Icons.info_outline),
+                            onPressed: () {
+                              showPreferenceInfo(preference);
+                            },
+                          ),
+                        ],
+                      ),
+                      value: selectedPreferences[preference],
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          selectedPreferences[preference] = newValue!;
+                        });
+                      },
+                    ),
                   );
                 }).toList(),
               ),
@@ -88,8 +119,17 @@ class PreferencesAccountState extends State<PreferencesAccount> {
             Center(
               child: ElevatedButton(
                 onPressed: () {
-                  Text(
-                      'Preferencias seleccionadas: ${selectedPreferences.toString()}');
+                  String selectedPrefs = selectedPreferences.entries
+                      .where((entry) => entry.value)
+                      .map((entry) => entry.key)
+                      .join(', ');
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content:
+                          Text('Preferencias seleccionadas: $selectedPrefs'),
+                    ),
+                  );
                 },
                 child: const Text('Guardar'),
               ),
