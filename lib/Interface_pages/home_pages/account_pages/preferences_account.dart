07512ffdcf1dbf937ecdc49a3ddbeb4f32853549
Coddy6 <asprans77@gmail.com>
2024-09-26 13:14:04 -0600

    Funcionamiento, selección de preferencias con la base de datos

--- lib/Interface_pages/home_pages/account_pages/preferences_account.dart
+++ lib/Interface_pages/home_pages/account_pages/preferences_account.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:eatsily/constants/constants.dart';
 
@@ -9,6 +11,7 @@ class PreferencesAccount extends StatefulWidget {
 }
 
 class PreferencesAccountState extends State<PreferencesAccount> {
+  User? user = FirebaseAuth.instance.currentUser;
   List<String> preferences = [
     'Omnívoro',
     'Vegano',
@@ -37,15 +40,7 @@ class PreferencesAccountState extends State<PreferencesAccount> {
         'La dieta carnívora se enfoca exclusivamente en alimentos de origen animal, excluyendo completamente los vegetales.'
   };
 
-  Map<String, bool> selectedPreferences = {};
-
-  @override
-  void initState() {
-    super.initState();
-    for (var preference in preferences) {
-      selectedPreferences[preference] = false;
-    }
-  }
+  String? selectedPreference;
 
   void showPreferenceInfo(String preference) {
     showDialog(
@@ -66,6 +61,117 @@ class PreferencesAccountState extends State<PreferencesAccount> {
         );
       },
     );
+  }
+
+  Future<void> updateUserPreferences(String userId) async {
+    // Estructura para almacenar los cambios a realizar en Firestore
+    List<String> allCategories = [
+      'Azúcares_y_dulces',
+      'Carnes_pescado_y_huevos',
+      'Cereales_y_tuberculos',
+      'Condimentos_y_salsas',
+      'Frutas',
+      'Grasas_y_aceites',
+      'Lacteos',
+      'Legumbres_y_frutos_secos',
+      'Verduras_y_hortalizas'
+    ];
+
+    DocumentSnapshot userDoc =
+        await FirebaseFirestore.instance.collection('Users').doc(userId).get();
+
+    if (userDoc.exists) {
+      Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+
+      Map<String, dynamic> resetValues = {};
+      for (String category in allCategories) {
+        if (userData[category] == -1) {
+          resetValues[category] = 0;
+        }
+      }
+
+      if (resetValues.isNotEmpty) {
+        // Actualizar categorías que estaban en -1 a 0
+        await FirebaseFirestore.instance
+            .collection('Users')
+            .doc(userId)
+            .update(resetValues);
+      }
+    }
+
+    Map<String, dynamic> updatedValues = {
+      'foodPreferences':
+          selectedPreference, // Guardar la preferencia en el campo 'foodPreferences'
+    };
+
+    switch (selectedPreference) {
+      case 'Omnívoro':
+        break;
+      case 'Vegano':
+        updatedValues
+            .addAll({'Azúcares_y_dulces': -1, 'Carnes_pescado_y_huevos': -1});
+        break;
+      case 'Vegetariano':
+        updatedValues
+            .addAll({'Azúcares_y_dulces': -1, 'Carnes_pescado_y_huevos': -1});
+        break;
+      case 'Flexitariana':
+        break;
+      case 'Pescetariana':
+        updatedValues.addAll({'Azúcares_y_dulces': -1});
+        break;
+      case 'Ovo-lacto Vegetariana':
+        updatedValues.addAll({'Azúcares_y_dulces': -11});
+        break;
+      case 'Carnívora':
+        updatedValues.addAll({
+          'Azúcares_y_dulces': -1,
+          'Cereales_y_tuberculos': -1,
+          'Condimentos_y_salsas': -1,
+          'Frutas': -1,
+          'Grasas_y_aceites': -1,
+          'Lacteos': -1,
+          'Legumbres_y_frutos_secos': -1,
+          'Verduras_y_hortalizas': -1
+        });
+        break;
+    }
+
+    // Actualizar Firestore con los valores modificados
+    try {
+      await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userId)
+          .update(updatedValues);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Preferencias guardadas correctamente')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error al guardar las preferencias')),
+      );
+    }
+  }
+
+  Future<void> loadUserPreferences(String userId) async {
+    DocumentSnapshot userDoc =
+        await FirebaseFirestore.instance.collection('Users').doc(userId).get();
+
+    if (userDoc.exists) {
+      Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+      String userPreference =
+          userData['foodPreferences'] ?? 'Omnívoro'; // Default value
+
+      setState(() {
+        selectedPreference = userPreference;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadUserPreferences(user!.uid); // Cargar preferencias guardadas al iniciar
   }
 
   @override
@@ -92,7 +198,7 @@ class PreferencesAccountState extends State<PreferencesAccount> {
                           color: const Color.fromARGB(255, 152, 102, 102)),
                       borderRadius: BorderRadius.circular(8),
                     ),
-                    child: CheckboxListTile(
+                    child: RadioListTile<String>(
                       title: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -105,10 +211,11 @@ class PreferencesAccountState extends State<PreferencesAccount> {
                           ),
                         ],
                       ),
-                      value: selectedPreferences[preference],
-                      onChanged: (bool? newValue) {
+                      value: preference,
+                      groupValue: selectedPreference,
+                      onChanged: (String? newValue) {
                         setState(() {
-                          selectedPreferences[preference] = newValue!;
+                          selectedPreference = newValue;
                         });
                       },
                     ),
@@ -119,17 +226,15 @@ class PreferencesAccountState extends State<PreferencesAccount> {
             Center(
               child: ElevatedButton(
                 onPressed: () {
-                  String selectedPrefs = selectedPreferences.entries
-                      .where((entry) => entry.value)
-                      .map((entry) => entry.key)
-                      .join(', ');
-
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content:
-                          Text('Preferencias seleccionadas: $selectedPrefs'),
-                    ),
-                  );
+                  if (selectedPreference != null) {
+                    updateUserPreferences(user!.uid);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Selecciona una preferencia'),
+                      ),
+                    );
+                  }
                 },
                 child: const Text('Guardar'),
               ),
