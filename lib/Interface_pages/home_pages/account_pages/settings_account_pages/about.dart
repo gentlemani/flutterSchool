bd51aa94e9b02d51ccbed31859597d476f658b4c
Coddy6 <asprans77@gmail.com>
2024-10-02 11:29:34 -0600

    Build widget about.dart

--- lib/Interface_pages/home_pages/account_pages/settings_account_pages/about.dart
+++ lib/Interface_pages/home_pages/account_pages/settings_account_pages/about.dart
@@ -1,3 +1,4 @@
+import 'package:eatsily/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class AboutEatsily extends StatelessWidget {
@@ -12,24 +13,14 @@ class AboutEatsily extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Sobre Eatsily',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            Text('Mejora tu cocina', style: headingTextStyle),
             SizedBox(height: 16),
             Text(
-              'Eatsily es tu asistente personal de cocina. Gracias a nuestra avanzada'
-              'tecnología de recomendación, Eatsily aprende de tus gustos y preferencias'
-              'para ofrecerte recetas personalizadas. Cada vez que interactúas con la app,'
-              'Eatsily ajusta sus sugerencias para brindarte platos que realmente disfrutarás,'
-              'basándose en ingredientes, tipos de cocina y categorías que te gustan. Ya'
-              'sea que quieras optimizar tu dieta o simplemente encontrar inspiración para'
-              'tus comidas diarias, Eatsily se adapta a tus necesidades. ¡Explora, cocina y'
-              'disfruta de una experiencia culinaria hecha a tu medida!',
-              style: TextStyle(fontSize: 16),
+              'Eatsily es tu asistente personal de cocina. Gracias a nuestra tecnología de recomendación, Eatsily aprende de tus gustos y preferencias '
+              'para ofrecerte recetas personalizadas. Cada vez que interactúas con la app, Eatsily ajusta sus sugerencias para brindarte platos que realmente disfrutarás, '
+              'basándose en ingredientes y categorías que te gustan. Ya sea que quieras optimizar tu dieta o simplemente encontrar inspiración para '
+              'tus comidas diarias, Eatsily se adapta a tus necesidades. ¡Explora, cocina y disfruta de una experiencia culinaria hecha a tu medida!',
+              style: bodyTextStyle,
             ),
           ],
         ),
